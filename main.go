b2a46e86485b1eaae28a147d2c680f382da38fc3
mudlyar-jayesh <mudliyar.jayesh@gmail.com>
2025-09-20 00:02:52 +0530

    Initial Commit

---
+++ main.go
@@ -0,0 +1,109 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"log"
+	"os"
+	"path/filepath"
+	"strings"
+	"sync"
+)
+
+type InvertedIndex map[string]map[string]struct{}
+
+type FileData struct {
+	Path    string
+	Content []byte
+}
+
+func findFiles(rootDir string, filePaths chan<- string, wg *sync.WaitGroup) {
+	defer wg.Done()
+	err := filepath.Walk(rootDir, func(path string, info os.FileInfo, err error) error {
+		if err != nil {
+			return err
+		}
+
+		if !info.IsDir() {
+			filePaths <- path
+		}
+		return nil
+	})
+	if err != nil {
+		log.Printf("Error walking directory: %v", err)
+	}
+	close(filePaths)
+}
+
+func readFile(filePaths <-chan string, fileContents chan<- FileData, wg *sync.WaitGroup) {
+	defer wg.Done()
+	for path := range filePaths {
+		content, err := os.ReadFile(path)
+		if err != nil {
+			log.Printf("Warning: Could not read file %s : %v", path, err)
+			continue
+		}
+		fileContents <- FileData{Path: path, Content: content}
+	}
+}
+
+func indexContent(fileContents <-chan FileData, index InvertedIndex, wg *sync.WaitGroup) {
+	defer wg.Done()
+	for data := range fileContents {
+		scanner := bufio.NewScanner(strings.NewReader(string(data.Content)))
+		scanner.Split(bufio.ScanWords)
+
+		for scanner.Scan() {
+			word := strings.ToLower(scanner.Text())
+
+			if _, ok := index[word]; !ok {
+				index[word] = make(map[string]struct{})
+			}
+
+			index[word][data.Path] = struct{}{}
+		}
+	}
+}
+
+func main() {
+	if len(os.Args) < 3 {
+		log.Fatalf("Usuage: %s <diretory> <search-term>", os.Args[0])
+	}
+
+	rootDir := os.Args[1]
+	searchTerm := strings.ToLower(os.Args[2])
+
+	var wg sync.WaitGroup
+	filePaths := make(chan string)
+	fileContents := make(chan FileData)
+
+	wg.Add(1)
+	go findFiles(rootDir, filePaths, &wg)
+
+	for i := 0; i < 10; i++ {
+		wg.Add(1)
+		go readFile(filePaths, fileContents, &wg)
+	}
+
+	var indexerWg sync.WaitGroup
+	indexerWg.Add(1)
+	index := make(InvertedIndex)
+	go indexContent(fileContents, index, &indexerWg)
+
+	wg.Wait()
+	close(fileContents)
+	indexerWg.Wait()
+
+	log.Println("Index built successfully")
+
+	results, found := index[searchTerm]
+	if !found {
+		fmt.Printf("\nNo results found for '%s'.\n", searchTerm)
+		return
+	}
+
+	fmt.Printf("\nFound '%s' in the following files:\n", searchTerm)
+	for path := range results {
+		fmt.Printf("- %s\n", path)
+	}
+}
